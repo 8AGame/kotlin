25088a5ec1d755186951bb669b6e05e8dfbeebe7
Valentin Kipyatkov <valentin@jetbrains.com>
2020-04-22 21:45:58 +0300

    Improvements after code review

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/search/ideaExtensions/KotlinReferenceScopeOptimizer.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/search/ideaExtensions/KotlinReferenceScopeOptimizer.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.idea.search.ideaExtensions
 
+import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.cache.CacheManager
 import com.intellij.psi.search.GlobalSearchScope
@@ -19,25 +20,30 @@ import org.jetbrains.kotlin.psi.KtFile
 
 class KotlinReferenceScopeOptimizer : ScopeOptimizer {
     override fun getRestrictedUseScope(element: PsiElement): SearchScope? {
-        if (element is KtCallableDeclaration) {
-            val file = element.parent as? KtFile ?: return null
-            val useScope = element.useScope as? GlobalSearchScope ?: return null
-            val packageName = file.packageFqName.takeUnless { it.isRoot } ?: return null
-            val project = file.project
-            val cacheManager = CacheManager.SERVICE.getInstance(project)
-
-            val kotlinScope = GlobalSearchScope.getScopeRestrictedByFileTypes(useScope, KotlinFileType.INSTANCE)
-            val nonKotlinScope = useScope.excludeFileTypes(KotlinFileType.INSTANCE) as GlobalSearchScope
-
-            //TODO: use all components of package name?
-            val shortPackageName = packageName.shortName().identifier
-            val kotlinFiles = cacheManager.getVirtualFilesWithWord(shortPackageName, UsageSearchContext.IN_CODE, kotlinScope, true)
-
-            val javaFacadeName = file.javaFileFacadeFqName.shortName().identifier
-            val nonKotlinFiles = cacheManager.getVirtualFilesWithWord(javaFacadeName, UsageSearchContext.IN_CODE, nonKotlinScope, true)
-            
-            return GlobalSearchScope.filesScope(project, (kotlinFiles + nonKotlinFiles).asList())
+        if (element is KtCallableDeclaration && element.parent is KtFile) {
+            return getRestrictedScopeForTopLevelCallable(element)
         }
         return null
+    }
+
+    private fun getRestrictedScopeForTopLevelCallable(callable: KtCallableDeclaration): GlobalSearchScope? {
+        val useScope = callable.useScope as? GlobalSearchScope ?: return null
+        val file = callable.parent as KtFile
+        val packageName = file.packageFqName.takeUnless { it.isRoot } ?: return null
+        val project = file.project
+        val cacheManager = CacheManager.SERVICE.getInstance(project)
+
+        val kotlinScope = GlobalSearchScope.getScopeRestrictedByFileTypes(useScope, KotlinFileType.INSTANCE)
+        val javaScope = GlobalSearchScope.getScopeRestrictedByFileTypes(useScope, JavaFileType.INSTANCE)
+        val restScope = useScope.excludeFileTypes(KotlinFileType.INSTANCE, JavaFileType.INSTANCE) as GlobalSearchScope
+
+        //TODO: use all components of package name?
+        val shortPackageName = packageName.shortName().identifier
+        val kotlinFiles = cacheManager.getVirtualFilesWithWord(shortPackageName, UsageSearchContext.IN_CODE, kotlinScope, true)
+
+        val javaFacadeName = file.javaFileFacadeFqName.shortName().identifier
+        val javaFiles = cacheManager.getVirtualFilesWithWord(javaFacadeName, UsageSearchContext.IN_CODE, javaScope, true)
+
+        return GlobalSearchScope.filesScope(project, (kotlinFiles + javaFiles).asList()).uniteWith(restScope)
     }
 }
